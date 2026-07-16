a066d4a1ccfafdcbcddc045755f324199bef1142
ngnd <bbcn419a@gmail.com>
2026-07-17 00:57:32 +0300

    exercise 7d

--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // strict mode has been tested, it helps us to catch bugs.
   <StrictMode>
     <BrowserRouter>
       <App />
