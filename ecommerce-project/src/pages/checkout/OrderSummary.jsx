cb5e140109f62342bb88f331f36ca89ca7720f35
ngnd <bbcn419a@gmail.com>
2026-07-17 00:54:10 +0300

    exercise 7c

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,26 +1,18 @@
 import { DeliveryOptions } from "./DeliveryOptions";
 import { CartItemDetails } from "./CartItemDetails";
-import dayjs from "dayjs";
+import { DeliveryDate } from "./DeliveryDate";
 
 export function OrderSummary({ deliveryOptions, cart }) {
     return (
         <div className="order-summary">
             {deliveryOptions?.length > 0 && cart.map((cartItem) => {
-                const selectedDeliveryOption = deliveryOptions
-                    .find((deliveryOption) => {
-                        return deliveryOption.id === cartItem.deliveryOptionId;
-                    });
 
                 return (
                     <div key={cartItem.id} className="cart-item-container">
-                        <div className="delivery-date">
-                            Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                        </div>
+                        <DeliveryDate deliveryOptions={deliveryOptions} cartItem={cartItem} />
 
                         <div className="cart-item-details-grid">
-
                             <OrderSummary cartItem={cartItem} />
-
                             <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
                         </div>
                     </div>
