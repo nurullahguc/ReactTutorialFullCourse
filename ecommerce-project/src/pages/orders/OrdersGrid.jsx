4a8109edd64d40f5bd00562dc986deef3f8dd3de
ngnd <bbcn419a@gmail.com>
2026-07-17 01:01:51 +0300

    exercise 7f

--- ecommerce-project/src/pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/pages/orders/OrdersGrid.jsx
@@ -1,35 +1,17 @@
 import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
 import BuyAgain from '../../assets/images/icons/buy-again.png'
 import { Fragment } from "react";
 import { Link } from "react-router";
+import { OrderHeader } from "./OrderHeader";
 
-export function OrdersGrid({orders}) {
+export function OrdersGrid({ orders }) {
     return (
         <div className="orders-grid">
             {orders.map((order) => {
                 return (
                     <div key={order.id} className="order-container">
 
-                        <div className="order-header">
-                            <div className="order-header-left-section">
-                                <div className="order-date">
-                                    <div className="order-header-label">Order Placed:</div>
-                                    <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
-                                </div>
-                                <div className="order-total">
-                                    <div className="order-header-label">Total:</div>
-                                    <div>
-                                        {formatMoney(order.totalCostCents)}
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="order-header-right-section">
-                                <div className="order-header-label">Order ID:</div>
-                                <div>{order.id}</div>
-                            </div>
-                        </div>
+                        <OrderHeader order={order} />
 
                         <div className="order-details-grid">
 
