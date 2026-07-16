1335c4d97e588d42954c41a56ac2b5f21a71bd79
ngnd <bbcn419a@gmail.com>
2026-07-16 21:10:21 +0300

    use data from the backend to generate HTML

--- ecommerce-project/src/pages/HomePages.jsx
+++ ecommerce-project/src/pages/HomePages.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import './HomePage.css'
 import Checkmark from '../assets/images/icons/checkmark.png'
-import { products } from '../../starting-code/data/products'
 
 export function HomePage() {
 
-    axios.get('http://localhost:3000/api/products')
-        .then((response) => {
-            console.log(response.data);
-        });
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data)
+            });
+    }, []);
 
     return (
         <>
@@ -18,7 +22,7 @@ export function HomePage() {
 
             <div className="home-page">
                 <div className="products-grid">
-                    {products.map((product) => {
+                    {products?.map((product) => {
                         return (
                             <div key={product.id} className="product-container">
                                 <div className="product-image-container">
