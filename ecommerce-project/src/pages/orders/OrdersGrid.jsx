45e283b399804669c887620c8b551abef59404ca
ngnd <bbcn419a@gmail.com>
2026-07-17 01:16:00 +0300

    exercise 7j

--- ecommerce-project/src/pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/pages/orders/OrdersGrid.jsx
@@ -37,7 +37,7 @@ export function OrdersGrid({ orders }) {
                                             </button>
                                         </div>
                                         <div className="product-actions">
-                                            <Link to={`/tracking${order.id}/${orderProduct.product.id}`}>
+                                            <Link to={`/tracking/${order.id}/${orderProduct.product.id}`}>
                                                 <button className="track-package-button button-secondary">
                                                     Track package
                                                 </button>
