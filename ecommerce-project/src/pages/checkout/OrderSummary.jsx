5f530a9d6b838a01cd2be3e4d620dd9cc05010c9
ngnd <bbcn419a@gmail.com>
2026-07-17 00:51:05 +0300

    exercise 7b

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,11 +1,11 @@
-import { formatMoney } from "../../utils/money";
-import dayjs from "dayjs";
 import { DeliveryOptions } from "./DeliveryOptions";
+import { CartItemDetails } from "./CartItemDetails";
+import dayjs from "dayjs";
 
 export function OrderSummary({ deliveryOptions, cart }) {
     return (
         <div className="order-summary">
-            {deliveryOptions.length > 0 && cart.map((cartItem) => {
+            {deliveryOptions?.length > 0 && cart.map((cartItem) => {
                 const selectedDeliveryOption = deliveryOptions
                     .find((deliveryOption) => {
                         return deliveryOption.id === cartItem.deliveryOptionId;
@@ -18,28 +18,8 @@ export function OrderSummary({ deliveryOptions, cart }) {
                         </div>
 
                         <div className="cart-item-details-grid">
-                            <img className="product-image"
-                                src={cartItem.product.image} />
 
-                            <div className="cart-item-details">
-                                <div className="product-name">
-                                    {cartItem.product.name}
-                                </div>
-                                <div className="product-price">
-                                    {formatMoney(cartItem.product.priceCents)}
-                                </div>
-                                <div className="product-quantity">
-                                    <span>
-                                        Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                                    </span>
-                                    <span className="update-quantity-link link-primary">
-                                        Update
-                                    </span>
-                                    <span className="delete-quantity-link link-primary">
-                                        Delete
-                                    </span>
-                                </div>
-                            </div>
+                            <OrderSummary cartItem={cartItem} />
 
                             <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
                         </div>
