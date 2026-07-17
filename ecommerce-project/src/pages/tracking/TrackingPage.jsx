e4a3c0aa03c649a8cb56595029cfc3008428b94e
ngnd <bbcn419a@gmail.com>
2026-07-17 21:18:22 +0300

    excercise 7k

--- ecommerce-project/src/pages/tracking/TrackingPage.jsx
+++ ecommerce-project/src/pages/tracking/TrackingPage.jsx
@@ -5,20 +5,31 @@ import { useParams } from "react-router";
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
+import dayjs from "dayjs";
 
 export function TrackingPage({ cart }) {
     const [order, setOrder] = useState(null);
+    const [product, setProduct] = useState(null);
     const { orderId, productId } = useParams();
-    //console.log(orderId, productId);
+    console.log(orderId, productId);
 
     useEffect(() => {
         const fetchTrackingData = async () => {
-            const resposne = await axios.get(`/api/orders/${orderId}?expand=products`);
-            setOrder(resposne.data);
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
         }
 
         fetchTrackingData();
     }, [orderId]);
+
+    useEffect(() => {
+        const findProduct = order?.products.find((productItem) => {
+            return productItem.productId === productId;
+        })
+        console.log("findProduct", findProduct);
+
+        setProduct(findProduct);
+    }, [order]);
 
     return (
         <>
@@ -31,35 +42,39 @@ export function TrackingPage({ cart }) {
                         View all orders
                     </Link>
 
-                    <div className="delivery-date">
-                        Arriving on Monday, June 13
-                    </div>
+                    {product && (
+                        <>
+                            <div className="delivery-date">
+                                Arriving on {dayjs(product.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+                            </div>
 
-                    <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
-                    </div>
+                            <div className="product-info">
+                                {product.product.name}
+                            </div>
 
-                    <div className="product-info">
-                        Quantity: 1
-                    </div>
+                            <div className="product-info">
+                                Quantity: {product.quantity}
+                            </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                            <img className="product-image" src={product.product.image} />
 
-                    <div className="progress-labels-container">
-                        <div className="progress-label">
-                            Preparing
-                        </div>
-                        <div className="progress-label current-status">
-                            Shipped
-                        </div>
-                        <div className="progress-label">
-                            Delivered
-                        </div>
-                    </div>
+                            <div className="progress-labels-container">
+                                <div className="progress-label">
+                                    Preparing
+                                </div>
+                                <div className="progress-label current-status">
+                                    Shipped
+                                </div>
+                                <div className="progress-label">
+                                    Delivered
+                                </div>
+                            </div>
 
-                    <div className="progress-bar-container">
-                        <div className="progress-bar"></div>
-                    </div>
+                            <div className="progress-bar-container">
+                                <div className="progress-bar"></div>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </>
