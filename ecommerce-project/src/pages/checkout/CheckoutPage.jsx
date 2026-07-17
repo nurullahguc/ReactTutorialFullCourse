e0e4b28c0b359ec1b57cb626115b27d115f79fb2
ngnd <bbcn419a@gmail.com>
2026-07-18 01:03:03 +0300

    exercise 8b

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -12,15 +12,21 @@ export function CheckoutPage({ cart, loadCart }) {
     const [paymentSummary, setPaymentSummary] = useState(null);
 
     useEffect(() => {
-        const fetchCheckoutData = async () => {
-            let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+        const fetchDeliveryOptions = async () => {
+            const response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
             setDeliveryOptions(response.data)
+        }
 
-            response = await axios.get('/api/payment-summary');
+        fetchDeliveryOptions();
+    }, []);
+
+    useEffect(() => {
+        const fetchPaymentSummary = async () => {
+            const response = await axios.get('/api/payment-summary');
             setPaymentSummary(response.data);
         }
 
-        fetchCheckoutData();
+        fetchPaymentSummary();
     }, [cart]);
 
     return (
