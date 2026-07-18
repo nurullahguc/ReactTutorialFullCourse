8560556d2cabcaf96c960270c3cd0e192adcf599
ngnd <bbcn419a@gmail.com>
2026-07-18 22:38:00 +0300

    exercise 9a

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -10,6 +10,7 @@ describe('formatMoney', () => {
     it('displays 2 decimals', () => {
         expect(formatMoney(1090)).toBe('$10.90')
         expect(formatMoney(100)).toBe('$1.00')
+        expect(formatMoney(0)).toBe('$0.00');
     });
 
 });
